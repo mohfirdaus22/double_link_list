374c903743fc92144e0e62cc4e2b4e4a3a890b59
mohfirdaus22 <thepashter6@gmail.com>
2022-11-18 17:18:34 +0700

    add the node at the end of list

--- double_link_list/Program.cs
+++ double_link_list/Program.cs
@@ -60,12 +60,27 @@ namespace double_link_list
             {
                 if(nim == current.noMhs)
                 {
-                    Console.WriteLine("\nDuplicate roll numbers not allowed);
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
                         return;
                 }
             }
+            /* On the execution of the above for loop, prev and
+            * current will point to those nodes
+            * between which the new node is to be inserted*/
+            newNode.next = current;
+            newNode.prev = previous;
+
+            //if the node is to be inserted at the end of the list
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
 
         }
-      
+
     }
 }
