023b8ad8df2a9bf234fdf85eb02fbc0e79428a5d
mohfirdaus22 <thepashter6@gmail.com>
2022-11-18 16:52:10 +0700

    Add class node

--- double_link_list/Program.cs
+++ double_link_list/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace double_link_list
+{
+    class Node
+
+    {
+        /*Node class represent the node of doubly linked list
+        *it consist of the information part and links to
+        *its succeding and preceeding
+        *in terms of the next and previouse*/
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //point to the preeceding node
+        public Node prev;
+    }
+
+}
