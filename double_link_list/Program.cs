d277119ed5f2484607cb27d00116247cbaba4c52
mohfirdaus22 <thepashter6@gmail.com>
2022-11-18 20:05:14 +0700

    Add console Writeline

--- double_link_list/Program.cs
+++ double_link_list/Program.cs
@@ -171,7 +171,18 @@ namespace double_link_list
             DoubleLinkedList obj = new DoubleLinkedList();
             while (true)
             {
-
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from to the list");
+                    Console.WriteLine("3. View all record in the asceding order of roll number");
+                    Console.WriteLine("4. View all record in  the descending order of roll number");
+                    Console.WriteLine("5. Search for a record in the list");
+                    Console.WriteLine("6. Exit\n");
+                    Console.WriteLine("Enter your choice (1-6): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                }
             }
         }
     }
